0ab204193ebea17a6513eadd2281bb1242bff2ae
komkem <komkem.contact@gmail.com>
2025-11-11 15:23:12 +0700

    feat: Add Vercel Analytics integration and update app icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// Vercel Analytics
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +18,14 @@ export const metadata: Metadata = {
   title: "Money Manage",
   description:
     "A simple money management app built with Next.js and Tailwind CSS.",
+  icons: {
+    icon: [
+      // { url: '/favicon.ico', sizes: 'any' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+      { url: '/favicon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
@@ -29,6 +39,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* Vercel Analytics component (client-side) */}
+        <Analytics />
       </body>
     </html>
   );
